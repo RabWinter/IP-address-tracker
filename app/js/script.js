c8171955211c2d6fc75436f1a34193fde54d4563
Rab Winter <77572162+RabWinter@users.noreply.github.com>
2021-04-04 15:58:05 +0100

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,21 @@
 'use strict';
 const personalApi = 'at_XVDfZ8UdXvLnfJRhn1z2jSPc6kGzk';
-// const apiUrl = 'https://www.ipify.org/';
-const bypassCorsUrl = 'cors-anywhere.herokuapp.com';
-
 const userIp = document.getElementById('ip-address');
 const userLocation = document.getElementById('location');
 const userTimezone = document.getElementById('timezone');
 const userIsp = document.getElementById('isp');
 
-const userInput = document.getElementById('search-input');
+const overlay = document.querySelector('.overlay');
+const errorContainer = document.getElementById('error');
+const errorMessage = document.getElementById('error__message');
+const closeErrorBtn = document.getElementById('close__error--btn');
+let userInput = document.getElementById('search-input');
 const searchBtn = document.querySelector('.search__button');
 
 let myMap = '';
+let myIcon = L.icon({
+ iconUrl: 'images/icon-location.svg',
+});
 
 // Render User Info
 function renderInfo(data) {
@@ -19,6 +23,25 @@ function renderInfo(data) {
  userLocation.innerHTML = `${data.location.city}, ${data.location.region}`;
  userTimezone.innerHTML = `${data.location.timezone}`;
  userIsp.innerHTML = `${data.isp}`;
+}
+
+function getMap(data) {
+ const lat = data.location.lat;
+ const lng = data.location.lng;
+
+ myMap = L.map('mapid').setView([lat, lng], 13);
+ L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicndpbnRlcjcxIiwiYSI6ImNrbjBoNnJ0ODBjbG0yb3BiaW54bzdobTUifQ.NTWRccBIgEerO4BIb96-AQ',
+  {
+   // attribution:
+   //  'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+   maxZoom: 20,
+   id: 'mapbox/streets-v11',
+   tileSize: 512,
+   zoomOffset: -1,
+  }
+ ).addTo(myMap);
+ L.marker([lat, lng], { icon: myIcon }).addTo(myMap);
 }
 
 function renderMap(data) {
@@ -30,14 +53,18 @@ function renderMap(data) {
   myMap.remove();
 
   myMap = L.map('mapid').setView([lat, lng], 13);
-  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-   attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(myMap);
-  L.marker([lat, lng])
-   .addTo(myMap)
-   .bindPopup('Here I am, which is here')
-   .openPopup();
+  L.tileLayer(
+   'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicndpbnRlcjcxIiwiYSI6ImNrbjBoNnJ0ODBjbG0yb3BiaW54bzdobTUifQ.NTWRccBIgEerO4BIb96-AQ',
+   {
+    // attribution:
+    //  'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 20,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+   }
+  ).addTo(myMap);
+  L.marker([lat, lng], { icon: myIcon }).addTo(myMap);
  }
 }
 
@@ -46,21 +73,8 @@ async function getIpData() {
  const ip = await fetch(`https://geo.ipify.org/api/v1?apiKey=${personalApi}`);
  const ipData = await ip.json();
  renderInfo(ipData);
-
- const lat = ipData.location.lat;
- const lng = ipData.location.lng;
-
- myMap = L.map('mapid').setView([lat, lng], 13);
-
- L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
- }).addTo(myMap);
-
- L.marker([lat, lng])
-  .addTo(myMap)
-  .bindPopup('Here I am, which is here')
-  .openPopup();
+ getMap(ipData);
+ //  renderMap(ipData);
 }
 
 getIpData();
@@ -72,16 +86,32 @@ async function search() {
    `https://geo.ipify.org/api/v1?apiKey=${personalApi}&domain=${userInput.value}`
   );
   const ipData = await ip.json();
-  renderMap(ipData);
   renderInfo(ipData);
+  // getMap(ipData);
+  renderMap(ipData);
  } catch (err) {
-  console.log(`Oops ${err}`);
+  displayErrorMsg(err);
  }
+}
+
+function displayErrorMsg() {
+ overlay.classList.add('fade-in');
+ errorContainer.classList.add('fade-in');
+ errorMessage.innerHTML = `Oops...Something went wrong. Please enter a valid IP address or domain`;
+}
+
+function removeErrorMsg() {
+ overlay.classList.remove('fade-in');
+ errorContainer.classList.remove('fade-in');
+ userInput.value = '';
 }
 
 // User Search
 searchBtn.addEventListener('click', (e) => {
  e.preventDefault();
  search();
- console.log('Searched');
+});
+
+closeErrorBtn.addEventListener('click', () => {
+ removeErrorMsg();
 });
